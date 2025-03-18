6c2430edd177c37c76229c33db86a5092a7cbb91
Talha Ahmad <talha.code92@gmail.com>
2025-03-18 17:28:55 +0500

    Update program.hs

--- Learning/Practice Questions/02 Sum of two input numbers/program.hs
+++ Learning/Practice Questions/02 Sum of two input numbers/program.hs
@@ -1,5 +1,5 @@
 {-
-Write a program to take two numbers and display their sum as:
+Write a program that take two numbers and display their sum as:
 Sum of these numbers is: <sum>
 -}
 
@@ -14,7 +14,7 @@ main = do
     num2 <- getLine
 
     -- Calculate the sum
-    let sum = (read num1 :: Int) + (read num2 :: Int)   -- read is used to convert string to integer
+    let sum = (read num1 :: Int) + (read num2 :: Int)   -- read is used to convert string to other (integer)
 
     -- Display the sum
-    putStrLn ("Sum of these numbers is: " ++ show sum)  -- show is used to convert integer to string
+    putStrLn ("Sum of these numbers is: " ++ show sum)  -- show is used to convert other (integer) to string
